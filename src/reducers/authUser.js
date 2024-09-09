30f04614b3142f67ff17acba6b26d4aaf8d1df57
DaNX2 <nxd113322@gmail.com>
2024-09-09 21:09:35 +0700

    Change logic reducers file

--- src/reducers/authUser.js
+++ src/reducers/authUser.js
@@ -1,42 +1,3 @@
-// import { LOGIN_AUTHED_USER, LOGOUT_AUTHED_USER } from '../actions/authedUser';
-
-
-// const authUser = (state = null, action) => {
-//   switch (action.type) {
-//     case LOGIN_AUTHED_USER:
-//       return action.authedUser;
-//     case LOGOUT_AUTHED_USER:
-//       return null;
-//     default:
-//       return state;
-//   }
-// };
-
-// export default authUser;
-
-//22222222222222222222222222222
-
-// import { LOGIN_AUTHED_USER, LOGOUT_AUTHED_USER } from '../actions/authedUser';
-
-// const initialState = null;
-
-// const authUser = (state = initialState, action) => {
-//   switch (action.type) {
-//     case LOGIN_AUTHED_USER:
-//       return action.payload;
-//     case LOGOUT_AUTHED_USER:
-//       return initialState;
-//     default:
-//       return state;
-//   }
-// };
-
-// export default authUser;
-
-
-
-///////////////////33333333333333333333333333
-
 import { LOGIN_AUTHED_USER, LOGOUT_AUTHED_USER } from '../actions/authedUser';
 
 const initialState = null;
@@ -48,7 +9,7 @@ const authenticatedUserReducer = (state = initialState, action) => {
     case LOGOUT_AUTHED_USER:
       return null;
     default:
-      return state; // Trả về trạng thái hiện tại nếu không xử lý loại hành động
+      return state; // Returns the current state if the action type is not processed
   }
 };
 
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,40 +1,3 @@
-// import {ADD_ANSWER_TO_QUESTION, CREATE_NEW_QUESTION, LOAD_QUESTIONS } from '../actions/polls';
-
-
-// const questions = (state = {}, action) => {
-//   switch (action.type) {
-//     case LOAD_QUESTIONS:
-//       return {
-//         ...state,
-//       };
-//     case CREATE_NEW_QUESTION:
-//       return {
-//         ...state,
-//         [action.question.id]: action.question,
-//       };
-//     case ADD_ANSWER_TO_QUESTION:
-//       return {
-//         ...state,
-//         [action.qid]: {
-//           ...state[action.qid],
-//           [action.answer]: {
-//             ...state[action.qid][action.answer],
-//             votes: state[action.qid][action.answer].votes.concat(
-//               action.authUser,
-//             ),
-//           },
-//         },
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export default questions;
-
-
-
-//---------------22222222222222222222222222222
 import { ADD_ANSWER_TO_QUESTION, CREATE_NEW_QUESTION, LOAD_QUESTIONS } from '../actions/polls';
 
 const initialState = {};
@@ -42,19 +5,19 @@ const initialState = {};
 const questionsReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_QUESTIONS:
-      // Cập nhật trạng thái với các câu hỏi nhận được
+      // Update status with questions received
       return {
         ...state,
         ...action.questions,
       };
     case CREATE_NEW_QUESTION:
-      // Thêm câu hỏi mới vào trạng thái
+      // Add new question to status
       return {
         ...state,
         [action.question.id]: action.question,
       };
     case ADD_ANSWER_TO_QUESTION:
-      // Cập nhật câu trả lời cho câu hỏi cụ thể
+      // Updated answer to specific question
       const { qid, answer, authedUser } = action;
       return {
         ...state,
